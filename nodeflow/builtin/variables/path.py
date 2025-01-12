dccb0c1302e5c20d51601e714f3d6393024b32a0
Maksim Shushkevich <83134047+meshushkevich@users.noreply.github.com>
2025-01-12 19:29:07 +0300

    Update path.py

--- nodeflow/builtin/variables/path.py
+++ nodeflow/builtin/variables/path.py
@@ -5,7 +5,6 @@ import pathlib
 class PathVariable(Variable):
     def __init__(self, value: pathlib.Path):
         assert isinstance(value, pathlib.Path)
-        assert value.exists()
         super().__init__(value.resolve())
 
     def __truediv__(self, other: str) -> pathlib.Path:
